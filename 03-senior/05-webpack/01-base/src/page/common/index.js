7c9033e9825117ad5ff89cb8a627c2a3c1f24840
TomChen <76876201@qq.com>
2019-04-08 21:01:15 +0800

    webpack

---
+++ 03-senior/05-webpack/01-base/src/page/common/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: TomChen
+* @Date:   2019-04-08 20:02:56
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-04-08 20:03:22
+*/
+require('./index.css')
+
+console.log('common js....')
---
+++ 03-senior/05-webpack/01-base/src/page/index/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: TomChen
+* @Date:   2019-04-08 20:03:55
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-04-08 20:04:11
+*/
+require('./index.css')
+
+console.log('index js....')
