141a4b8d8f51678dc190a66a1b706ceb590bdba0
TomChen <76876201@qq.com>
2019-04-24 18:27:10 +0800

    reset css and top nav ui for kmall

--- 04-project/kmall/home/src/pages/common/index.js
+++ 04-project/kmall/home/src/pages/common/index.js
@@ -2,8 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-23 19:38:42
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-23 20:44:53
+* @Last Modified time: 2019-04-24 18:06:34
 */
 require('node_modules/font-awesome/css/font-awesome.min.css')
 require('./index.css')
-console.log('common js...')
--- 04-project/kmall/home/src/pages/common/nav/index.js
+++ 04-project/kmall/home/src/pages/common/nav/index.js
@@ -1,7 +1,7 @@
 /*
 * @Author: TomChen
-* @Date:   2019-04-23 19:31:31
+* @Date:   2019-04-24 18:17:34
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-24 18:06:49
+* @Last Modified time: 2019-04-24 18:18:07
 */
 require('./index.css')
