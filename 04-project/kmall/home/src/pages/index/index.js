d7487f5bc09f4aa27c7b1ad680e1c7ee652197e3
TomChen <76876201@qq.com>
2019-04-26 19:24:11 +0800

    swiper for index page

--- 04-project/kmall/home/src/pages/index/index.js
+++ 04-project/kmall/home/src/pages/index/index.js
@@ -2,8 +2,11 @@
 * @Author: TomChen
 * @Date:   2019-04-23 19:24:03
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-26 19:07:44
+* @Last Modified time: 2019-04-26 19:21:57
 */
+import Swiper from 'swiper'
+import 'swiper/dist/css/swiper.min.css'
+
 require('pages/common/nav')
 require('pages/common/search')
 require('pages/common/footer')
@@ -26,12 +29,30 @@ var page = {
 	],
 	init:function(){
 		this.loadKeywords();
+		this.loadSwiper();
 	},
 	loadKeywords:function(){
 		var html = _util.render(keywordsTpl,{
 			keywords:this.keywords
 		});
 		$('.keywords').html(html)
+	},
+	loadSwiper:function(){
+		var mySwiper = new Swiper ('.swiper-container', {
+			loop: true, // 循环模式选项
+
+			// 如果需要分页器
+			pagination: {
+			  el: '.swiper-pagination',
+			  clickable:true
+			},
+
+			// 如果需要前进后退按钮
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+		})
 	}
 }
 $(function(){
