6b975f11fb7994eb783e87d71408e0685da39861
TomChen <76876201@qq.com>
2019-04-28 11:36:03 +0800

    pagination ui

--- 04-project/kmall/home/src/util/pagination/index.js
+++ 04-project/kmall/home/src/util/pagination/index.js
@@ -2,9 +2,11 @@
 * @Author: TomChen
 * @Date:   2019-04-28 10:44:41
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-28 10:54:55
+* @Last Modified time: 2019-04-28 11:34:40
 */
-require('./index.css')
+require('./index.css');
+var _util = require('util');
+var tpl = require('./index.tpl');
 
 ;(function($){
 	function Pagination($elem){
@@ -17,10 +19,47 @@ require('./index.css')
 
 		},
 		render:function(options){
-			console.log(options)
+			//1.计算总页数
+			var pages = Math.ceil(options.total/options.pageSize)
+			if(pages <= 1){
+				return;
+			}
+			//上一页 1 2 3 *4 5 6 7 下一页
+			//上一页 2 3 4 *5 6 7 8 下一页
+			//上一页 5 6 7 *8 9 10 11 下一页
+			var start = options.current - options.range > 1 ? (options.current - options.range) : 1
+			var end = options.current + options.range > pages ? pages : (options.current + options.range)
+			var prev = options.current  - 1;
+			var next = options.current + 1;
+			var pageArray = [];
+
+			pageArray.push({
+				name:'上一页',
+				value:prev,
+				disabled:prev == 0 ? true : false
+			})
+
+			for(var i = start; i<=end;i++){
+				pageArray.push({
+					name:i,
+					value:i,
+					active:options.current == i
+				})
+			}
+			pageArray.push({
+				name:'下一页',
+				value:next,
+				disabled:next > pages ? true : false
+			})
+			var html = _util.render(tpl,{
+				pageArray:pageArray
+			})
+			this.$elem.html(html)
 		}
 	}
-
+	Pagination.DEFAULT = {
+		range:3
+	}
 	//注册插件
 	$.fn.extend({
 		pagination:function(fn,options){
@@ -32,6 +71,7 @@ require('./index.css')
 					$this.data('pagination',pagination)
 				}
 				if(typeof pagination[fn] == 'function'){
+					options = $.extend({},Pagination.DEFAULT,options)
 					pagination[fn](options)
 				}
 
--- 04-project/kmall/server/util/pagination.js
+++ 04-project/kmall/server/util/pagination.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2018-08-09 10:22:53
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-26 20:38:29
+* @Last Modified time: 2019-04-28 11:16:04
 */
 
 
@@ -33,7 +33,7 @@ let pagination = (options)=>{
 		}
 
 		//每页显示条数
-		let limit = 10;
+		let limit = 2;
 
 		/*
 		分页:
