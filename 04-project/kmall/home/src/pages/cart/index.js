bc93c5667777f3f6be0caf0ed4046fe7980e3151
TomChen <76876201@qq.com>
2019-04-29 18:22:24 +0800

    cart select one

--- 04-project/kmall/home/src/pages/cart/index.js
+++ 04-project/kmall/home/src/pages/cart/index.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-23 19:31:31
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-28 17:27:03
+* @Last Modified time: 2019-04-29 18:20:24
 */
 require('pages/common/nav')
 require('pages/common/search')
@@ -22,24 +22,47 @@ var page = {
 	loadCart:function(){
 		var _this = this;
 		_cart.getCart(function(cart){
-			if(cart.cartList.length>0){
-				//处理图片
-				cart.cartList.forEach(function(item){
-					item.product.mainImg = item.product.images.split(',')[0]
-				})
-				console.log(cart);
-				var html = _util.render(tpl,cart);
-				_this.$elem.html(html);
-			}else{
-				_this.$elem.html('<p class="empty-msg">购物车空空如也!</p>')	
-			}
+			_this.renderCart(cart);
 		},function(){
 			_this.$elem.html('<p class="empty-msg">加载购物车失败!稍后再试</p>')	
 		})
 
 	},
+	renderCart:function(cart){
+		if(cart.cartList.length>0){
+			//处理图片
+			cart.cartList.forEach(function(item){
+				item.product.mainImg = item.product.images.split(',')[0]
+			})
+			var html = _util.render(tpl,cart);
+			this.$elem.html(html);
+		}else{
+			this.$elem.html('<p class="empty-msg">购物车空空如也!</p>')	
+		}
+	},
 	bindEvent:function(){
 		var _this = this;
+		//1.单条商品选中/取消
+		this.$elem.on('click','.select-one',function(){
+			var $this = $(this);
+			var productId = $this.parents('.product-item').data('product-id')
+			//选中
+			if($this.is(":checked")){
+				_cart.selectOne({productId:productId},function(cart){
+					_this.renderCart(cart);
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})
+			}
+			//取消
+			else{
+				_cart.unselectOne({productId:productId},function(cart){
+					_this.renderCart(cart);
+				},function(msg){
+					_util.showErrorMsg(msg)
+				})				
+			}
+		})
 	}
 
 }
--- 04-project/kmall/home/src/service/cart/index.js
+++ 04-project/kmall/home/src/service/cart/index.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-24 19:11:15
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-28 17:20:31
+* @Last Modified time: 2019-04-29 18:13:49
 */
 var _util = require('util')
 
@@ -30,6 +30,24 @@ var _cart = {
 			error:error			
 		})			
 	},
+	selectOne:function(data,success,error){
+		_util.request({
+			url:'/cart/selectOne',
+			method:'put',
+			data:data,
+			success:success,
+			error:error			
+		})		
+	},
+	unselectOne:function(data,success,error){
+		_util.request({
+			url:'/cart/unselectOne',
+			method:'put',
+			data:data,
+			success:success,
+			error:error			
+		})		
+	},		
 }
 
 module.exports = _cart;
