5831d2649484b5443ffe56fdd9818335c3bb828e
TomChen <76876201@qq.com>
2019-05-05 10:12:45 +0800

    load order detail

--- 04-project/kmall/home/src/pages/order-detail/index.js
+++ 04-project/kmall/home/src/pages/order-detail/index.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-23 19:31:31
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-05-05 09:17:23
+* @Last Modified time: 2019-05-05 10:10:24
 */
 require('pages/common/nav')
 require('pages/common/search')
@@ -26,6 +26,31 @@ var page = {
 	},
 	loadOrderDetail:function(){
 		var _this = this;
+		if(this.params.orderNo){
+			_order.getOrder(this.params,function(order){
+				_this.renderOrderDetail(order)
+			},function(msg){
+				_this.$elem.html('<p class="empty-msg">获取订单失败</p>')
+			})			
+		}else{
+			_this.$elem.html('<p class="empty-msg">订单号不存在</p>')
+		}
+
+	},
+	renderOrderDetail:function(order){
+		if(order){
+			//适配数据
+			order.productList.forEach(function(product){
+				product.image = product.images.split(',')[0]
+			})
+			order.createdTime = new Date(order.createdAt).toLocaleString()			
+			order.canPay = order.canCancel = order.status == '10'
+
+			var html = _util.render(tpl,order)
+			this.$elem.html(html)
+		}else{
+			this.$elem.html('<p class="empty-msg">订单不存在</p>')
+		}
 	}
 }
 $(function(){
--- 04-project/kmall/home/src/service/order/index.js
+++ 04-project/kmall/home/src/service/order/index.js
@@ -2,7 +2,7 @@
 * @Author: TomChen
 * @Date:   2019-04-24 19:11:15
 * @Last Modified by:   TomChen
-* @Last Modified time: 2019-04-30 20:44:27
+* @Last Modified time: 2019-05-05 09:53:49
 */
 var _util = require('util')
 
@@ -30,7 +30,15 @@ var _order = {
 			success:success,
 			error:error			
 		})			
-	}			
+	},
+	getOrder:function(data,success,error){
+		_util.request({
+			url:'/order/home/detail',
+			data:data,
+			success:success,
+			error:error			
+		})			
+	},		
 }
 
 module.exports = _order;
