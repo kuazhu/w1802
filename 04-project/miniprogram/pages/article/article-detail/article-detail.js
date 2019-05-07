562719ba036f49cf3bd4be49f831e00ff360dd6e
TomChen <76876201@qq.com>
2019-05-07 18:51:33 +0800

    load data for detail mp;

--- 04-project/miniprogram/pages/article/article-detail/article-detail.js
+++ 04-project/miniprogram/pages/article/article-detail/article-detail.js
@@ -1,3 +1,4 @@
+var { articles } = require('../../../data/db.js')
 Page({
 
   /**
@@ -12,55 +13,8 @@ Page({
    */
   onLoad: function (options) {
     var articleId = options.articleId;
-    console.log(articleId)
+    var article = articles[articleId];
+    this.setData({ ...article})
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
 })
