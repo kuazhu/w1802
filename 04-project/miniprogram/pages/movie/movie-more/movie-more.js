fcd10c0233cfa6b76e58bc9da834360ff699da21
TomChen <76876201@qq.com>
2019-05-09 18:41:18 +0800

    reach bottom and load more

--- 04-project/miniprogram/pages/movie/movie-more/movie-more.js
+++ 04-project/miniprogram/pages/movie/movie-more/movie-more.js
@@ -6,9 +6,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    requestUrl:''
+    requestUrl:'',
+    totalCount:0,
+    totalData:[],
+    isEnd:false
   },
-
+  handleMovieListData:function(data){
+    wx.hideNavigationBarLoading();
+    if(data.length == 0){
+      wx.showToast({
+        title: '底库都拉出来啦',
+      })
+      this.data.isEnd = true
+      return;
+    }
+    this.data.totalCount = this.data.totalCount + data.length;
+    this.data.totalData = this.data.totalData.concat(data);
+    this.setData({
+      movies: this.data.totalData
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -38,11 +55,15 @@ Page({
     wx.setNavigationBarTitle({
       title: title,
     })
+    /*
     getMovieListData(requestUrl,function(data){
+      _this.data.totalCount = data.length;
       _this.setData({
         movies:data
       })
     })
+    */
+    getMovieListData(requestUrl, this.handleMovieListData);   
   },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
@@ -50,7 +71,9 @@ Page({
    */
   onPullDownRefresh: function () {
     var _this = this;
+    wx.showNavigationBarLoading();
     getMovieListData(this.data.requestUrl, function (data) {
+      wx.hideNavigationBarLoading();
       _this.setData({
         movies: data
       })
@@ -62,6 +85,14 @@ Page({
    * 获取更多
    */
   onReachBottom: function () {
-
+    if (this.data.isEnd) {
+      wx.showToast({
+        title: '底库都拉出来啦',
+      })
+      return;
+    }    
+    var nextUrl = this.data.requestUrl +'?start='+this.data.totalCount+'&count=20';
+    wx.showNavigationBarLoading();
+    getMovieListData(nextUrl, this.handleMovieListData);    
   },
 })
